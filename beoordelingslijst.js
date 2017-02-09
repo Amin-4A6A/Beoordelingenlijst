b28e8afab3ce563844c09c25f61143b1d2f5953a
Amin-4A6A <510074@edu.rocmn.nl>
2017-02-09 11:49:07 +0100

    dingen toegevoegt

--- beoordelingslijst.js
+++ beoordelingslijst.js
@@ -1,23 +1,26 @@
 function voegToe() {
-  var cijfer =
-  document.getElementById("input").value
-
-
+  var cijfer = document.getElementById("input").value
   if (cijfer >= 0 && cijfer <= 49) {
-    document.getElementById('resultaat').innerHTML +="het cijfer is "+cijfer+" onvoldoende";
-  }
-  else if
-    (cijfer >= 50 && cijfer <= 59) {
-        document.getElementById('resultaat').innerHTML +="het cijfer is "+cijfer+" matig";
-  }
-  else if
-    (cijfer >= 60 && cijfer <= 74) {
-        document.getElementById('resultaat').innerHTML +="het cijfer is "+cijfer+" goed";
-  }
-  else if
-    (cijfer >= 75 && cijfer <= 100) {
-        document.getElementById('resultaat').innerHTML +="het cijfer is "+cijfer+" goed";
+    document.getElementById('resultaat').innerHTML +="het cijfer is "+cijfer+" onvoldoende <br>";
+  } else if (cijfer >= 50 && cijfer <= 59) {
+        document.getElementById('resultaat').innerHTML +="het cijfer is "+cijfer+" matig <br>";
+  } else if (cijfer >= 60 && cijfer <= 74) {
+        document.getElementById('resultaat').innerHTML +="het cijfer is "+cijfer+" voldoende <br>";
+  } else if (cijfer >= 75 && cijfer <= 100) {
+        document.getElementById('resultaat').innerHTML +="het cijfer is "+cijfer+" goed <br>";
   }
 
 
+}
+function volledigeCijfer() {
+  var cijfer1 = document.getElementById("input1").value;
+  if (cijfer1 >= 0 && cijfer1 <= 49) {
+    document.getElementById('resultaat').innerHTML +="De beoordeling is onvoldoende, want het cijfer is "+cijfer1+".  <br>";
+  } else if (cijfer1 >= 50 && cijfer1 <= 59) {
+        document.getElementById('resultaat').innerHTML +="De beoordeling is matig, want het cijfer is "+cijfer1+".  <br>";
+  } else if (cijfer1 >= 60 && cijfer1 <= 74) {
+        document.getElementById('resultaat').innerHTML +="De beoordeling is voldoende, want het cijfer is "+cijfer1+".  <br>";
+  } else if (cijfer1 >= 75 && cijfer1 <= 100) {
+        document.getElementById('resultaat').innerHTML +="De beoordeling is Goed, want het cijfer is "+cijfer1+".  <br>";
+  }
 }
